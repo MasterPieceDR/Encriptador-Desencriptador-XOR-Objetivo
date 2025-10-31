5f7ec1885116d4cf7f14c24a62caea40dd67925d
MasterPieceDR <diegosantillan625@gmail.com>
2025-10-31 12:41:01 -0500

    Refactor XOR cipher into a function

--- xor_cipher.py
+++ xor_cipher.py
@@ -1,6 +1,10 @@
 import random
-t = input("Texto: ")
-k = ''.join(str(random.randint(0,1)) for _ in t)
-c = ''.join(chr(ord(t[i]) ^ int(k[i])) for i in range(len(t)))
-d = ''.join(chr(ord(c[i]) ^ int(k[i])) for i in range(len(c)))
-print("Texto plano:", t, "\nLlave:", k, "\nTexto encriptado:", c, "\nTexto desencriptado:", d)
+
+def xor_cipher():
+    t = input("Texto: ")
+    k = ''.join(str(random.randint(0,1)) for _ in t)
+    c = ''.join(chr(ord(t[i]) ^ int(k[i])) for i in range(len(t)))
+    d = ''.join(chr(ord(c[i]) ^ int(k[i])) for i in range(len(c)))
+    print("Texto plano:", t, "\nLlave:", k, "\nTexto encriptado:", c, "\nTexto desencriptado:", d)
+
+xor_cipher()
